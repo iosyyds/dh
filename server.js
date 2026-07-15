d4ebdcd4238c5a2dcc7dc59888159462e752c05c
甜甜 <145662294+iosyyds@users.noreply.github.com>
2026-07-16 00:02:45 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+﻿const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const BASE = "E:/mistora/project/官网1";
+const MIME = {
+  ".html": "text/html; charset=utf-8",
+  ".css": "text/css",
+  ".js": "application/javascript",
+  ".gif": "image/gif",
+  ".png": "image/png",
+  ".jpg": "image/jpeg",
+  ".svg": "image/svg+xml",
+  ".ico": "image/x-icon",
+  ".woff2": "font/woff2",
+  ".woff": "font/woff",
+  ".ttf": "font/ttf"
+};
+
+http.createServer((req, res) => {
+  let filePath = path.join(BASE, req.url === "/" ? "index.html" : req.url.split("?")[0]);
+  const ext = path.extname(filePath).toLowerCase();
+  fs.readFile(filePath, (err, data) => {
+    if (err) { res.writeHead(404); res.end("Not found"); return; }
+    res.writeHead(200, { "Content-Type": MIME[ext] || "application/octet-stream" });
+    res.end(data);
+  });
+}).listen(3000, () => console.log("Server running at http://localhost:3000"));
